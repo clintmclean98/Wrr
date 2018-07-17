6771b2bdfa5ea2c5fa6f662f4c2f2e12f4ac8109
Clint <Clintjoshuamclean@gmail.com>
2018-07-17 20:40:43 +0200

    Trying out this shit

---
+++ app/src/main/java/t/flatearchsocie/database/AddInfo.java
@@ -0,0 +1,25 @@
+package t.flatearchsocie.database;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Clint on 2018/07/17.
+ */
+
+class AddInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void AddRecord(View view){
+
+
+    }
+}
